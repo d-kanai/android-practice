2b659721d5b90dd14f019631ff5b0dd944a59612
d.kanai <contact@dkanai.com>
2022-04-03 08:57:32 +0900

    list view

---
+++ app/src/main/java/com/example/android_practice/DiceActivity.kt
@@ -0,0 +1,59 @@
+package com.example.android_practice
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+
+const val EXTRA_MESSAGE = "message"
+
+class DiceActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        Log.v("APP_LOG", "Hello, world!")
+        diceRoll()
+        rollButton.setOnClickListener {
+            Log.v("APP_LOG", "dice roll")
+            diceRoll()
+        }
+    }
+
+    private fun diceRoll() {
+        val resultTextView: TextView = (findViewById(R.id.roll_text))
+        val diceValue = Dice(6).roll()
+        resultTextView.text = diceValue.toString()
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        val drawableResource = when (diceValue) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+        Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
+    }
+
+    fun sendMessage(view: View) {
+        println("click button")
+        val editText = findViewById<EditText>(R.id.editTextTextPersonName)
+        val message = editText.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
+    }
+}
+
+class Dice(private val numSides: Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}
--- app/src/main/java/com/example/android_practice/MainActivity.kt
+++ app/src/main/java/com/example/android_practice/MainActivity.kt
@@ -1,58 +1,19 @@
 package com.example.android_practice
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.view.View
-import android.widget.*
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 
-const val EXTRA_MESSAGE = "message"
-
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val rollButton: Button = findViewById(R.id.roll_button)
-        Log.v("APP_LOG", "Hello, world!")
-        diceRoll()
-        rollButton.setOnClickListener {
-            Log.v("APP_LOG", "dice roll")
-            diceRoll()
-        }
+        val data = arrayOf("Coverage", "Long Method")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, data)
+        val listView = findViewById<ListView>(R.id.dod_list_view)
+        listView.adapter = adapter
     }
 
-    private fun diceRoll() {
-        val resultTextView: TextView = (findViewById(R.id.roll_text))
-        val diceValue = Dice(6).roll()
-        resultTextView.text = diceValue.toString()
-        val diceImage: ImageView = findViewById(R.id.dice_image)
-        val drawableResource = when (diceValue) {
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            else -> R.drawable.dice_6
-        }
-        diceImage.setImageResource(drawableResource)
-        Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
-    }
-
-    fun sendMessage(view: View) {
-        println("click button")
-        val editText = findViewById<EditText>(R.id.editTextTextPersonName)
-        val message = editText.text.toString()
-        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
-            putExtra(EXTRA_MESSAGE, message)
-        }
-        startActivity(intent)
-    }
-}
-
-class Dice(private val numSides: Int) {
-
-    fun roll(): Int {
-        return (1..numSides).random()
-    }
 }
