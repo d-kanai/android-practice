86f99afb574ba66b02ab7ff38ae9b9d28c133e9e
d.kanai <contact@dkanai.com>
2022-04-02 20:18:12 +0900

    set value to text view

--- app/src/main/java/com/example/android_practice/MainActivity.kt
+++ app/src/main/java/com/example/android_practice/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.android_practice
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 const val EXTRA_MESSAGE = "message"
 
@@ -12,6 +15,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener {
+            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
+            toast.show()
+            val resultTextView: TextView = findViewById(R.id.roll_text)
+            resultTextView.text = "6"
+        }
     }
 
     fun sendMessage(view: View) {
