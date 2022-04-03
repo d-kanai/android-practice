236f7aa8b31283aa3a2670a851a65419ede76a4b
d.kanai <contact@dkanai.com>
2022-04-03 10:39:12 +0900

    remove count

--- app/src/main/java/com/example/android_practice/MainActivity.kt
+++ app/src/main/java/com/example/android_practice/MainActivity.kt
@@ -10,13 +10,11 @@ import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
-import com.example.android_practice.viewmodels.CountViewModel
 import com.example.android_practice.viewmodels.DoDViewModel
 
 class MainActivity : AppCompatActivity() {
 
     private val viewModel: DoDViewModel by viewModels()
-    private val countViewModel: CountViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,15 +24,10 @@ class MainActivity : AppCompatActivity() {
 
         viewModel.dodList.observe(this, Observer { dodList ->
             Log.v("APP_LOG", "publish dodList")
-            val a = dodList!!.map { dod -> dod.name }?.toList()
-            listView.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, a)
+            val names = dodList!!.map { dod -> dod.name }?.toList()
+            listView.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, names)
         })
 
-        countViewModel.count.observe(this, Observer { count ->
-            Log.v("APP_LOG", "publish count")
-            findViewById<TextView>(R.id.count_text).text = count.toString()
-        })
-        findViewById<TextView>(R.id.count_up_button).setOnClickListener { countViewModel.countUp() }
     }
 
     private fun onClickListItem(view: View) {
