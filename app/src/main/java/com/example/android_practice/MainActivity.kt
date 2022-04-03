2d41c687ba2ebbbba224ae0110231f04c66fb2a6
d.kanai <contact@dkanai.com>
2022-04-03 10:30:51 +0900

    dodList view model

--- app/src/main/java/com/example/android_practice/MainActivity.kt
+++ app/src/main/java/com/example/android_practice/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.android_practice
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.ListView
@@ -15,24 +16,25 @@ import com.example.android_practice.repositories.DoDRepository
 
 class MainActivity : AppCompatActivity() {
 
+    private val viewModel: DoDViewModel by viewModels()
+    private val countViewModel: CountViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val listView = findViewById<ListView>(R.id.dod_list_view)
         listView.setOnItemClickListener { parent, view, position, id -> onClickListItem(view) }
 
-        val viewModel: DoDViewModel by viewModels()
-        viewModel.load()
-        val dodList = viewModel.findDoDList()
-        val a = dodList.map { dod -> dod.name }.toList()
-        listView.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, a)
+        viewModel.dodList.observe(this, Observer { dodList ->
+            Log.v("APP_LOG", "publish dodList")
+            val a = dodList!!.map { dod -> dod.name }?.toList()
+            listView.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, a)
+        })
 
-        val countViewModel: CountViewModel by viewModels()
-        countViewModel.countValue.observe(this,
-            Observer { countValue ->
-                findViewById<TextView>(R.id.count_text).text = countValue.toString()
-            }
-        )
+        countViewModel.count.observe(this, Observer { count ->
+            Log.v("APP_LOG", "publish count")
+            findViewById<TextView>(R.id.count_text).text = count.toString()
+        })
         findViewById<TextView>(R.id.count_up_button).setOnClickListener { countViewModel.countUp() }
     }
 
@@ -45,15 +47,15 @@ class MainActivity : AppCompatActivity() {
 }
 
 class DoDViewModel : ViewModel() {
-    private var dodList: List<DoD> = listOf()
+    val dodList: MutableLiveData<List<DoD>> by lazy { MutableLiveData<List<DoD>>() }
 
-    fun load() {
-        val dodList = DoDRepository().findDoDList()
-        this.dodList = dodList
+    init {
+        load()
     }
 
-    fun findDoDList(): List<DoD> {
-        return dodList
+    private fun load() {
+        val dodList = DoDRepository().findDoDList()
+        this.dodList.value = dodList
     }
 
 }
@@ -64,13 +66,18 @@ class DoD(val name: String) {
 
 
 class CountViewModel : ViewModel() {
-    var countValue = MutableLiveData(0)
+    val count: MutableLiveData<Int> by lazy { MutableLiveData<Int>() }
+
+    init {
+        initCount()
+    }
+
+    private fun initCount() {
+        count.value = 100
+    }
 
     fun countUp() {
-        countValue.value = countValue.value!!.plus(1)
+        count.value = count.value!!.plus(1)
     }
 
-    fun getCount(): String {
-        return countValue.value.toString()
-    }
 }
