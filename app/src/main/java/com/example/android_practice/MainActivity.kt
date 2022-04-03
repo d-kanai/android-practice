fc9e434ee827a39de8476ea0ced954ed71652117
d.kanai <contact@dkanai.com>
2022-04-03 11:57:41 +0900

    WIP listview with databinding

--- app/src/main/java/com/example/android_practice/MainActivity.kt
+++ app/src/main/java/com/example/android_practice/MainActivity.kt
@@ -1,15 +1,11 @@
 package com.example.android_practice
 
 import android.os.Bundle
-import android.util.Log
-import android.widget.ArrayAdapter
+import android.widget.AdapterView
 import android.widget.ListView
-import android.widget.TextView
-import android.widget.Toast
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.BindingAdapter
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Observer
 import com.example.android_practice.databinding.ActivityMainBinding
 import com.example.android_practice.viewmodels.CountViewModel
 import com.example.android_practice.viewmodels.DoDViewModel
@@ -17,35 +13,19 @@ import com.example.android_practice.viewmodels.DoDViewModel
 
 class MainActivity : AppCompatActivity() {
 
-    private val viewModel: DoDViewModel by viewModels()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
             .apply {
                 lifecycleOwner = this@MainActivity
                 countViewModel = CountViewModel()
+                dodViewModel = DoDViewModel()
             }
-
-        val listView = findViewById<ListView>(R.id.dod_list_view)
-        listView.setOnItemClickListener { parent, view, position, id ->
-            Log.v("System", "on click listener")
-            viewModel.onClickListItem(view)
-            val item = (view.findViewById<TextView>(android.R.id.text1)).text
-            Toast.makeText(applicationContext, item, Toast.LENGTH_SHORT).show()
-        }
-        viewModel.dodList.observe(this, Observer { dodList ->
-            Log.v("System", "publish dodList")
-            val names = dodList!!.map { dod -> dod.name }?.toList()
-            Log.v("System", names.toString())
-            listView.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, names)
-        })
-
     }
-
-
 }
 
-
+@BindingAdapter("onItemClickListener")
+fun setOnItemClickListener(listView: ListView, listener: AdapterView.OnItemClickListener) {
+    listView.onItemClickListener = listener
+}
--- app/src/main/java/com/example/android_practice/viewmodels/DoDViewModel.kt
+++ app/src/main/java/com/example/android_practice/viewmodels/DoDViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.android_practice.viewmodels
 
 import android.view.View
+import android.widget.AdapterView
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.android_practice.models.DoD
@@ -16,6 +17,15 @@ class DoDViewModel : ViewModel() {
     private fun load() {
         println("load DoD List")
         this.dodList.value = DoDRepository().findDoDList()
+    }
+
+    fun getOnItemClickListener(): AdapterView.OnItemClickListener {
+        return AdapterView.OnItemClickListener { adapterView, view, i, l ->
+            println("on click trigger from xml")
+            val dodList = this.dodList.value
+            dodList?.add(DoD("APPEND"))
+            this.dodList.value = dodList
+        }
     }
 
     fun onClickListItem(view: View?) {
