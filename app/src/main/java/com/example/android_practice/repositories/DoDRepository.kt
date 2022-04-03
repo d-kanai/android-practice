fd94e02c22dc1b37eb2baf2b8cff8eac8d0e2629
d.kanai <contact@dkanai.com>
2022-04-03 15:07:09 +0900

    call api

--- app/src/main/java/com/example/android_practice/repositories/DoDRepository.kt
+++ app/src/main/java/com/example/android_practice/repositories/DoDRepository.kt
@@ -1,9 +1,24 @@
 package com.example.android_practice.repositories
 
 import com.example.android_practice.models.DoD
+import okhttp3.*
+import java.io.IOException
 
 class DoDRepository {
     fun findDoDList(): MutableList<DoD> {
+        val client = OkHttpClient()
+        val url: String = "http://192.168.10.109:9000/dods"
+        val request = Request.Builder().url(url).get().build()
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                println("fail : $e")
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                println(call)
+                println(response.body?.string())
+            }
+        })
         return mutableListOf(DoD("Long Method"), DoD("Coverage"))
     }
 }
