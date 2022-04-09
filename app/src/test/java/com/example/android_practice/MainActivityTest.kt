4812f5508a422450347a7182ea6c9bab0de7106e
d.kanai <contact@dkanai.com>
2022-04-09 17:18:10 +0900

    wire mock

--- app/src/test/java/com/example/android_practice/MainActivityTest.kt
+++ app/src/test/java/com/example/android_practice/MainActivityTest.kt
@@ -3,7 +3,12 @@ package com.example.android_practice
 import android.widget.ListView
 import android.widget.TextView
 import com.example.android_practice.models.DoD
+import com.example.android_practice.repositories.DoDListResponse
+import com.github.tomakehurst.wiremock.client.WireMock.*
+import com.github.tomakehurst.wiremock.junit.WireMockRule
+import com.google.gson.Gson
 import junit.framework.Assert.assertEquals
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.Robolectric
@@ -11,35 +16,41 @@ import org.robolectric.RobolectricTestRunner
 import org.robolectric.RuntimeEnvironment
 import org.robolectric.Shadows.shadowOf
 
+
 @RunWith(RobolectricTestRunner::class)
 class MainActivityTest {
+
+    @Rule
+    @JvmField
+    var wireMockRule = WireMockRule(9000)
 
     @Test
     fun loadDoDListWhenOnCreate() {
         //given
-        //TODO: turn on PowerMockito
-//        val pmock = PowerMockito.mock(DoDRepository::class.java)
-//        `when`(pmock.findDoDList() {})
-        val activity: MainActivity = Robolectric.setupActivity(MainActivity::class.java);
+        stubDoDListApi(DoDListResponse(mutableListOf(DoD("Long Method"))))
         //when
+        val activity: MainActivity = Robolectric.setupActivity(MainActivity::class.java);
         val listViewText = activity.findViewById<TextView>(R.id.tvName)
         //then
-        assertEquals("Coverage", listViewText.text)
+        assertEquals("Long Method", listViewText.text)
     }
 
     @Test
     fun moveDoDDetailPageWhenTapDoDListItem() {
         //given
-//        val pmock = PowerMockito.mock(DoDRepository::class.java)
-//        `when`(pmock.findDoDList(){})
-        val activity: MainActivity = Robolectric.setupActivity(MainActivity::class.java);
+        stubDoDListApi(DoDListResponse(mutableListOf(DoD("Long Method"))))
         //when
+        val activity: MainActivity = Robolectric.setupActivity(MainActivity::class.java);
         val listView = activity.findViewById<ListView>(R.id.dod_list_view)
         listView.performItemClick(listView.getChildAt(0), 0, listView.getChildAt(0).id.toLong());
         //then
         val actual = shadowOf(RuntimeEnvironment.application).nextStartedActivity
         assertEquals("com.example.android_practice.DodDetailActivity", actual.component!!.className)
-        assertEquals("Coverage", (actual.extras!!["dod"] as DoD).name)
+        assertEquals("Long Method", (actual.extras!!["dod"] as DoD).name)
+    }
+
+    private fun stubDoDListApi(dodListApiResponse: DoDListResponse) {
+        stubFor(get("/dods").willReturn(ok().withBody(Gson().toJson(dodListApiResponse))))
     }
 }
 
