86f7281917048b0a20672a3c8d5f4525fbf8e7bf
d.kanai <contact@dkanai.com>
2022-07-03 14:36:41 +0900

    set wiremock stub from junit

--- integrationtest/src/test/java/cucumber/steps/BaseStep.java
+++ integrationtest/src/test/java/cucumber/steps/BaseStep.java
@@ -1,25 +1,29 @@
 package cucumber.steps;
 
+import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import cucumber.AndroidApp;
 import io.cucumber.java.en.Given;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.get;
-import static com.github.tomakehurst.wiremock.client.WireMock.ok;
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
 
 public class BaseStep {
 
-    public WireMockRule wireMockRule = new WireMockRule(9000);
+    WireMockServer wireMockServer = new WireMockServer(9000);
 
     @Given("Open App")
     public void openApp() {
         AndroidApp.launch();
+        wireMockServer.start();
     }
 
     @Given("Set stub response")
     public void setStubResponse() {
-        wireMockRule.stubFor(get("/dods")
+        wireMockServer.stubFor(get("/dods")
                 .willReturn(ok()
                         .withBody("{\"items\":[]}")));
+        wireMockServer.stubFor(post("/dods")
+                .willReturn(ok()
+                        .withBody("{\"name\":\"Long Method\"}")));
     }
 }
